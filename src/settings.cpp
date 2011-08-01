80e36611c1604276c353d4d4617f1fd58a5409d4
Popa Adrian Marius (Mariuz) <mapopa@gmail.com>
2011-08-01 16:55:58 +0300

    disable access keys by default, it is hard to use them when you do ctrl-c ctrl-v

--- src/settings.cpp
+++ src/settings.cpp
@@ -283,7 +283,7 @@ void SettingsDialog::loadFromSettings()
     // Accessibility
 #if QT_VERSION >= 0x040600 || defined(WEBKIT_TRUNK)
     settings.beginGroup(QLatin1String("WebView"));
-    enableAccessKeys->setChecked(settings.value(QLatin1String("enableAccessKeys"), true).toBool());
+    enableAccessKeys->setChecked(settings.value(QLatin1String("enableAccessKeys"), false).toBool());
     settings.endGroup();
 #else
     enableAccessKeys->setEnabled(false);
